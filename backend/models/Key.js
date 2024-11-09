6808af5d09a04aca175f54b58339a53da3538b41
gagenikolov33 <gagenikolov33@gmail.com>
2024-11-09 22:33:24 +0100

    0

--- backend/models/Key.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const KeySchema = new mongoose.Schema({
-    privateKey: { type: String, required: true },
-    address: { type: String, required: true },
-});
-
-module.exports = mongoose.model('Key', KeySchema);
